7bf10bc6e42786592ef38cd8fa62c1134c6c7306
dollemika <dollemika@mail.ru>
2024-04-03 15:40:46 +0300

    решение байдарки

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,38 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
+        int n,maxWeight;
         Scanner read = new Scanner(System.in);
-        int n = read.nextInt();
-        int numberOfDays = 0, j=0;
-        for (int i=0;i<n;i++){
-            if (read.nextInt()>0) j++;
-            else {
-                numberOfDays = Math.max(numberOfDays,j);
-                j = 0;
+        n = read.nextInt();
+        maxWeight = read.nextInt();
+        int[] array = new int[n];
+        int[] taken = new int[n];
+        for (int i = 0; i<n; i++)
+            array[i]= read.nextInt();
+        Arrays.sort(array);
+        int numBay=0;
+
+//        for (int i = array.length-1; i>=0; i--){
+//            if (taken[i] == 1)
+//                continue;
+//            for (int j = i-1; j>=0; j--)
+//                if (array[j]+array[i]<=maxWeight && taken[j]==0){
+//                    taken[j]=1;
+//                    break;
+//                }
+//            numBay++;
+//        }
+        int j = 0;
+        for (int i = array.length-1; i>=0; i--){
+            if (taken[i] == 1)
+                continue;
+            if (array[j]+array[i]<=maxWeight && taken[j]==0) {
+                taken[j] = 1;
+                j++;
             }
+            numBay++;
         }
-        System.out.println(numberOfDays);
+        System.out.println(numBay);
     }
-    //решена задача
-    //осталось сделать ничего
 }
