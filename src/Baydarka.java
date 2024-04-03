fc4dd6c0ae213d3a7d7177c4ee13803075e94eab
dollemika <dollemika@mail.ru>
2024-04-03 16:53:26 +0300

    плюс робот

---
+++ src/Baydarka.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Baydarka {
+    public static void main(String[] args) {
+        int n,maxWeight;
+        Scanner read = new Scanner(System.in);
+        n = read.nextInt();
+        maxWeight = read.nextInt();
+        int[] array = new int[n];
+        int[] taken = new int[n];
+        for (int i = 0; i<n; i++)
+            array[i]= read.nextInt();
+        Arrays.sort(array);
+        int numBay=0;
+
+//        for (int i = array.length-1; i>=0; i--){
+//            if (taken[i] == 1)
+//                continue;
+//            for (int j = i-1; j>=0; j--)
+//                if (array[j]+array[i]<=maxWeight && taken[j]==0){
+//                    taken[j]=1;
+//                    break;
+//                }
+//            numBay++;
+//        }
+        int j = 0;
+        for (int i = array.length-1; i>=0; i--){
+            if (taken[i] == 1)
+                continue;
+            if (array[j]+array[i]<=maxWeight && taken[j]==0) {
+                taken[j] = 1;
+                j++;
+            }
+            numBay++;
+        }
+        System.out.println(numBay);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,71 @@
 import java.util.Scanner;
-import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int n,maxWeight;
+        int i,j,kolS;
+        int q = 0-1;
+        boolean l,r,u,d,marker=false;
+        boolean[][] field = new boolean[101][101];
         Scanner read = new Scanner(System.in);
-        n = read.nextInt();
-        maxWeight = read.nextInt();
-        int[] array = new int[n];
-        int[] taken = new int[n];
-        for (int i = 0; i<n; i++)
-            array[i]= read.nextInt();
-        Arrays.sort(array);
-        int numBay=0;
-
-//        for (int i = array.length-1; i>=0; i--){
-//            if (taken[i] == 1)
-//                continue;
-//            for (int j = i-1; j>=0; j--)
-//                if (array[j]+array[i]<=maxWeight && taken[j]==0){
-//                    taken[j]=1;
-//                    break;
-//                }
-//            numBay++;
-//        }
-        int j = 0;
-        for (int i = array.length-1; i>=0; i--){
-            if (taken[i] == 1)
-                continue;
-            if (array[j]+array[i]<=maxWeight && taken[j]==0) {
-                taken[j] = 1;
-                j++;
+        String str = read.nextLine();
+        i = j = 50;
+        kolS=0;
+        field[i][j]=true;
+        u=true;
+        d=false;
+        l=false;
+        r=false;
+        for(int k=0;k<str.length()-1;k++)
+        {
+            if (str.charAt(k)=='S'){
+                if (u) i--;
+                else
+                    if (d) i++;
+                    else
+                        if (r) j++;
+                        else j--;
+                kolS++;
+                if (field[i][j]){
+                    System.out.println(kolS);
+                    marker = true;
+                break;}
+                else field[i][j]=true;
             }
-            numBay++;
+            else
+                if (str.charAt(k)=='L') {
+                    if (u) {
+                        u = false;
+                        l = true;
+                    } else if (d) {
+                        d = false;
+                        r = true;
+                    } else if (r) {
+                        r = false;
+                        u = true;
+                    } else {
+                        l = false;
+                        d = true;
+                    }
+                }
+                else {
+                    if (u) {
+                        u = false;
+                        r = true;
+                    } else if (d) {
+                        d = false;
+                        l = true;
+                    } else if (r) {
+                        r = false;
+                        d = true;
+                    } else {
+                        l = false;
+                        u = true;
+                    }
+                }
+
         }
-        System.out.println(numBay);
+        if(!marker)
+            System.out.println(q);
+
     }
 }
